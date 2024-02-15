1786f67681f4b92ba8f878e92286b2cb1223df7d
paulafiaschi <pau.fiaschi@gmail.com>
2024-02-15 16:23:49 +0100

    search function working

--- app/components/Table.jsx
+++ app/components/Table.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Ficha from "./Ficha";
 import FilterButton from "./FilterButton";
 import SubfilterButton from "./SubfilterButton";
@@ -12,6 +12,10 @@ export default function Main(props) {
   const [subcategories, setSubcategories] = useState([new Set()]);
   const [subcategory, setSubcategory] = useState("");
   const [subfilteredList, setSubfilteredList] = useState([]);
+
+  const [isSearching, setIsSearching] = useState(false);
+  const [searchedResults, setSearchedResults] = useState([]);
+  const searchGroup = useRef();
 
   useEffect(() => {
     setSubcategories([]);
@@ -44,8 +48,51 @@ export default function Main(props) {
       : categories.push(ficha.Categoría);
   });
 
+  function searchInfo(e) {
+    const input = e.target.value.toLowerCase();
+    const searchedResults = props.data.filter((item) => {
+      return (
+        item.Nombre.toLowerCase().includes(input) ||
+        item.Categoría.toLowerCase().includes(input)
+      );
+    });
+    console.log("searched", searchedResults);
+    // searchedProducts.length === 0 ? setNoProductsFound(true) : setNoProductsFound(false);
+
+    input === "" ? setSearchedResults([]) : setSearchedResults(searchedResults);
+    // console.log(input);
+  }
+
   return (
     <>
+      {/* Search starts */}
+      <div className="search">
+        <label className="input input-bordered flex items-center gap-2 w-96">
+          <input
+            type="text"
+            className="grow"
+            placeholder="Buscar..."
+            id="searchBar"
+            ref={searchGroup}
+            onChange={searchInfo}
+          />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            className="w-4 h-4 opacity-70"
+          >
+            <path
+              fillRule="evenodd"
+              d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </label>
+      </div>
+
+      {/* Search ends */}
+
       <h2 className="text-4xl pl-3 font-bold mb-2">Categorías</h2>
       <div className="filters menu lg:menu-horizontal gap-3 mb-2 w-full">
         {categories.map((c, i) => {
