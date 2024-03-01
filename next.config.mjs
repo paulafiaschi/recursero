0cce8cbc9ef0047986adeb68b69ee13cb8bb4c7c
paulafiaschi <pau.fiaschi@gmail.com>
2024-03-01 15:31:30 +0100

    redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+module.exports = {
+    trailingSlash: true,
+      async rewrites() {
+        return [
+          {
+            source: '',
+            destination: 'https://lasotras.dk/recursero/'
+          },
+        ];
+      },
+    };
+
 export default nextConfig;
