5f1c15576b5ab24323b6ecd7a87801768037d4db
paulafiaschi <pau.fiaschi@gmail.com>
2024-03-01 16:08:09 +0100

    modified next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = {
-    trailingSlash: true,
-      async rewrites() {
-        return [
-          {
-            source: '',
-            destination: 'https://lasotras.dk/recursero/'
-          },
-        ];
-      },
-    };
 
 export default nextConfig;
